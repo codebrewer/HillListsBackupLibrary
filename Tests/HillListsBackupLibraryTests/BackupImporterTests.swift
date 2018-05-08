589753854a7059f7fb32ea9a920cc1efd8f3c4f2
Mark Scott <mark@codebrewer.org>
2018-05-08 16:43:08 +0100

    Account for changed build output directory structure
    The Swift Package Manager still doesn't have support for bundling and
    locating test resources and the workaround to use the observed directory
    structure needs tweaking to allow for an extra directory level
    introduced between Swift 3.1 and 4.1.
    
    #4

--- Tests/HillListsBackupLibraryTests/BackupImporterTests.swift
+++ Tests/HillListsBackupLibraryTests/BackupImporterTests.swift
@@ -54,6 +54,7 @@ class BackupImporterTests: XCTestCase {
                 "..",
                 "..",
                 "..",
+                "..",
                 "Tests",
                 "HillListsBackupLibraryTests",
                 "Data",
