5121c577c783caeac4107ec4f738e691746504b5
Mark Scott <mark@codebrewer.org>
2018-05-08 16:46:27 +0100

    Regenerate the Package.swift manifest file
    Swift tools version 4 generates a manifest file that differs quite
    significantly from that generated by version 3.
    
    #4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,24 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "HillListsBackupLibrary"
+        name: "HillListsBackupLibrary",
+        products: [
+          .library(
+                  name: "HillListsBackupLibrary",
+                  targets: ["HillListsBackupLibrary"]),
+        ],
+        dependencies: [
+        ],
+        targets: [
+          .target(
+                  name: "HillListsBackupLibrary",
+                  dependencies: [],
+                  path: "Sources"),
+          .testTarget(
+                  name: "HillListsBackupLibraryTests",
+                  dependencies: ["HillListsBackupLibrary"]),
+        ]
 )
